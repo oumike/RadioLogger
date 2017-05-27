1dfa5659b0edafaec8700fd47fbbc5a0500d2518
Michael A. Cojocari <michael.cojocari@gmail.com>
2017-05-27 14:51:56 -0400

    Stuff.

--- app/Http/Controllers/ObservationController.php
+++ app/Http/Controllers/ObservationController.php
@@ -16,7 +16,6 @@ class ObservationController extends BaseController
 	public function index()
 	{
 
-	    // I've added some comments.
 		$stations = $this->createSelectArray(Station::all());
 		$radioShows = $this->createSelectArrayWithBlank(RadioShow::all());
 		$schedules = $this->createSelectArrayWithBlank(Schedule::all());
